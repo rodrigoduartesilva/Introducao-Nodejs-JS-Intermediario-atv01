08644057b3b88393d7ff33c9f8e873d23aacd0e6
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-09-03 12:43:25 -0300

    feat: implementacao da funcao de exibir menu de opcoes

---
+++ funcoes/exibirMenu.js
@@ -0,0 +1,16 @@
+function exibirMenu() {
+    console.log('\n');
+    console.log('************************ Menu ************************');
+    console.log('\n');
+
+    console.log('1 - Imprimir os dados do usuário detalhadamente');
+    console.log('2 - Imprimir os dados do usuário em formato String');
+    console.log('3 - Imprimir os dados do usuário em formato de objeto');
+    console.log('4 - Finalizar a aplicação');
+
+    console.log('\n');
+    console.log('******************************************************');
+    console.log('\n');
+}
+
+module.exports = exibirMenu;
