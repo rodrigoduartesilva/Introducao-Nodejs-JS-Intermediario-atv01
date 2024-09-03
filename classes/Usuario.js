dac4eb498ba3a4a0d37fdd45f8cc3b868146e937
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-09-03 09:25:19 -0300

    feat: classe Usuario implementada.

---
+++ classes/Usuario.js
@@ -0,0 +1,25 @@
+const { Pessoa } = require("./Pessoa");
+const anoAtual = 2024;
+
+class Usuario extends Pessoa {
+    constructor(nome, anoDeNascimento, sexo, nacionalidade, email, senha) {
+        super(nome, anoDeNascimento, sexo, nacionalidade);
+
+        this.nome = nome,
+            this.anoDeNascimento = anoDeNascimento,
+            this.sexo = sexo,
+            this.nacionalidade = nacionalidade,
+            this.email = email,
+            this.semja = senha
+    }
+
+    idadeEstimada = () => {
+        return anoAtual - this.anoDeNascimento;
+    }
+
+    info = () => {
+        return ` Dados do usuário: \n\n Nome: ${this.nome} \n Ano de Nascimento: ${this.anoDeNascimento} \n Idade Estimada: ${this.idadeEstimada()} anos \n Sexo: ${this.sexo} \n Nacionalidade: ${this.nacionalidade} \n Email: ${this.email}`;
+    }
+}
+
+module.exports = { Usuario };
