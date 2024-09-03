48da049811e4fc887b1727832ecfe0ddcee9765d
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-09-03 20:51:54 -0300

    fix: correcao da expressao regular de verificacao do texto.

--- funcoes/verificarEntradaDeTexto.js
+++ funcoes/verificarEntradaDeTexto.js
@@ -1,6 +1,6 @@
 // Função que retorna uma expressao regular afim de conferir se o dado digitado é compativel com o modelo esperado
 function verificarEntradaDeTexto(valor) {
-    return /^[a-zA-Z]+$/.test(valor);
+    return /^[a-z A-Z]+$/.test(valor);
 }
 
 module.exports = verificarEntradaDeTexto;
