aad25532736d6edbbb17290c075d791693707659
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-09-03 18:23:35 -0300

    feat: inserido tratamento de excecoes para os valores.

--- funcoes/informarDadosDoUsuario.js
+++ funcoes/informarDadosDoUsuario.js
@@ -1,20 +1,24 @@
 const prompt = require('prompt-sync')();
 const { Usuario } = require("../classes/Usuario.js");
+const solicitarEntrada = require('./solicitarEntrada.js');
+const tratarExcecaoDeEmail = require('./tratarExcecaoDeEmail.js');
+const tratarExcecaoDeTexto = require('./tratarExcecaoDeTexto.js');
+const tratarExcecaoNumerica = require('./tratarExcecaoNumerica.js');
+const verificarEntradaDeEmail = require('./verificarEntradaDeEmail.js');
+const verificarEntradaDeTexto = require('./verificarEntradaDeTexto.js');
+const verificarEntradaNumerica = require('./verificarEntradaNumerica.js');
 
 function informarDadosDoUsuario() {
-    let nome;
-    let anoDeNascimento;
-    let sexo;
-    let nacionalidade;
-    let email;
-    let senha;
+    console.log('\n');
+    console.log('*************** Sistema para cadastro de usuário ***************');
+    console.log('\n');
 
-    nome = prompt('Informe o nome do usuário: ');
-    anoDeNascimento = +prompt('Informe o ano de nascimento do usuário: ');
-    sexo = prompt('Informe o sexo do usuário: ');
-    nacionalidade = prompt('Informe a nacionalidade do usuário: ');
-    email = prompt('Informe o email do usuário: ');
-    senha = prompt('Informe a senha do usuário: ');
+    const nome = solicitarEntrada('Informe o nome do usuário: ', tratarExcecaoDeTexto, verificarEntradaDeTexto);
+    const anoDeNascimento = solicitarEntrada('Informe o ano de nascimento do usuário: ', tratarExcecaoNumerica, verificarEntradaNumerica);
+    const sexo = solicitarEntrada('Informe o sexo do usuário: ', tratarExcecaoDeTexto, verificarEntradaDeTexto);
+    const nacionalidade = solicitarEntrada('Informe a nacionalidade do usuário: ', tratarExcecaoDeTexto, verificarEntradaDeTexto);
+    const email = solicitarEntrada('Informe o email do usuário: ', tratarExcecaoDeEmail, verificarEntradaDeEmail);
+    const senha = prompt('Informe a senha do usuário: ');
 
     const usuario = new Usuario(nome, anoDeNascimento, sexo, nacionalidade, email, senha);
 
